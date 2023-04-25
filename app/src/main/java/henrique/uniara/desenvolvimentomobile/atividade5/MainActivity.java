05c7c1c46fe45f310f11cbb605fed873cf5f9d8e
Henrique Aquino <93733820+HenriQuest-DATA@users.noreply.github.com>
2023-04-25 14:48:26 -0300

    Notificação e envio de SMS com Android Studio em Java

---
+++ app/src/main/java/henrique/uniara/desenvolvimentomobile/atividade5/MainActivity.java
@@ -0,0 +1,109 @@
+package henrique.uniara.desenvolvimentomobile.atividade5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
+    String CHANNEL_ID = "channelid";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                String[] permissions = {android.Manifest.permission.SEND_SMS};
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }
+
+        createNotificationChannel();
+        addBotaoNotificationClickListener();
+    }
+
+    public void addBotaoNotificationClickListener() {
+        Button btnNotify = (Button) findViewById(R.id.btnNotification);
+        Button btnSMS = (Button) findViewById(R.id.btnSMS);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                callNotification();
+            }
+        });
+
+        btnSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendSMS();
+            }
+        });
+    }
+
+    public void sendSMS() {
+        EditText phoneNumberInput = findViewById(R.id.phoneNumber);
+        String phone = getValueFromInput(phoneNumberInput);
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phone, null, "sms test", null, null);
+            Toast.makeText(getApplicationContext(), "Mensagem enviada com sucesso", Toast.LENGTH_SHORT).show();
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public String getValueFromInput(EditText CampoTexto) {
+
+        return CampoTexto.getText().toString();
+    }
+
+    public void callNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("Notificação")
+                .setContentText("Clicou! Uhuuu!!!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        notificationManager.notify(1234, builder.build());
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+}
